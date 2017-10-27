2531d71ea8aefce4c938d90d6d5482b40d335a23
Nico De Witte <nico.dewitte@vives.be>
2017-10-27 21:36:16 +0200

    Add some basic validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ApplicationRecord
+  validates :name, length: { maximum: 64 }, presence: true, uniqueness: true
+  validates :auth_key, length: { is: 8 }, presence: true, uniqueness: true
+  has_many :messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  belongs_to :client
+  validates :client_id, presence: true
+  validates :content, presence: true
+  validates :severity, presence: true, inclusion: { in: %w( verbose info debug warning error ) }
 end
