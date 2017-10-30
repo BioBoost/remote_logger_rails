05d136b312b18a33b15d78cb01df3486f80fc834
Nico De Witte <nico.dewitte@vives.be>
2017-10-30 09:53:50 +0100

    Allow messages via json and not via html

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :force_json, except: [:index, :show]
 
   # GET /messages
   # GET /messages.json
@@ -12,42 +13,18 @@ class MessagesController < ApplicationController
   def show
   end
 
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit
-  end
-
-  # POST /messages
   # POST /messages.json
   def create
     @message = Message.new(message_params)
+    # if (request.format.symbol == :json)
+    client = Client.find_by_auth_key(params[:auth_key])
+    @message.client = client
+    # end
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
+    if @message.save
+      render :show, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
     end
   end
 
@@ -70,5 +47,9 @@ class MessagesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:client_id, :content, :severity, :logtime)
+    end
+
+    def force_json
+      render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
     end
 end
