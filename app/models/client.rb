8baa5f73ac8665963e2ef2e52e7f8b1ba792d698
Nico De Witte <nico.dewitte@vives.be>
2017-10-30 10:19:37 +0100

    Add number of different message types

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,16 @@ class Client < ApplicationRecord
   validates :name, length: { maximum: 64 }, presence: true, uniqueness: true
   validates :auth_key, length: { is: 8 }, presence: true, uniqueness: true
   has_many :messages, dependent: :destroy
+
+  def number_of_errors
+    return messages.where(:severity => :error).count
+  end
+
+  def number_of_warnings
+    return messages.where(:severity => :warning).count
+  end
+
+  def number_of_messages
+    return messages.count
+  end
 end
